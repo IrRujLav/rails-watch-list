02216ba27dcb70d3004db65f514f2b6d1635e769
Irene Rujas Lavandera <pacinha84@gmail.com>
2024-03-13 14:57:44 +0100

    Added bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,17 +1,20 @@
 class BookmarksController < ApplicationController
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     @bookmark.save
-    redirect_to lists_path(@list)
+    redirect_to list_path(@list)
   end
 
   private
 
-  def strong_params
+  def bookmark_params
     params.require(:bookmark).permit(:list_id, :movie_id, :comment)
   end
 end
